99cdb0f9fc138bfa97b9d856440b04f7dd9af7b1
b1n <120295547+b1nhack@users.noreply.github.com>
2024-05-10 02:07:42 +0800

    feat(cmp): add icons for completion-menu

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -15,6 +15,34 @@ return {
 		local cmp = require("cmp")
 		local luasnip = require("luasnip")
 
+		local kind_icons = {
+			Text = "",
+			Method = "󰆧",
+			Function = "󰊕",
+			Constructor = "",
+			Field = "󰇽",
+			Variable = "󰂡",
+			Class = "󰠱",
+			Interface = "",
+			Module = "",
+			Property = "󰜢",
+			Unit = "",
+			Value = "󰎠",
+			Enum = "",
+			Keyword = "󰌋",
+			Snippet = "",
+			Color = "󰏘",
+			File = "󰈙",
+			Reference = "",
+			Folder = "󰉋",
+			EnumMember = "",
+			Constant = "󰏿",
+			Struct = "",
+			Event = "",
+			Operator = "󰆕",
+			TypeParameter = "󰅲",
+		}
+
 		cmp.setup({
 			sources = {
 				{ name = "nvim_lsp" },
@@ -74,6 +102,22 @@ return {
 
 			view = {
 				entries = { name = "custom", selection_order = "near_cursor" },
+			},
+
+			formatting = {
+				format = function(entry, vim_item)
+					-- Kind icons
+					vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind) -- This concatenates the icons with the name of the item kind
+					-- Source
+					vim_item.menu = ({
+						buffer = "[Buffer]",
+						nvim_lsp = "[LSP]",
+						luasnip = "[Snip]",
+						path = "[Path]",
+						cmdline = "[Cmdline]",
+					})[entry.source.name]
+					return vim_item
+				end,
 			},
 
 			-- https://github.com/hrsh7th/nvim-cmp/pull/676#issuecomment-1002532096
