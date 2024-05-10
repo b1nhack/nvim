6398d638873b6e2f5160877304a389403cb17ec2
b1n <120295547+b1nhack@users.noreply.github.com>
2024-05-11 03:42:11 +0800

    feat(treesitter): jump code context with "[c"

---
+++ lua/plugins/nvim-treesitter-context.lua
@@ -0,0 +1,9 @@
+return {
+	"nvim-treesitter/nvim-treesitter-context",
+
+	config = function()
+		vim.keymap.set("n", "[c", function()
+			require("treesitter-context").go_to_context(vim.v.count1)
+		end, { silent = true })
+	end,
+}
--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -2,7 +2,7 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	dependencies = {
-		"nvim-treesitter/nvim-treesitter-context",
+		"nvim-treesitter-context",
 	},
 	event = { "BufReadPost", "BufWritePost", "BufNewFile" },
 
