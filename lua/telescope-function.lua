7f764211a73bc32f30e53b3f75ab60343b34b3a2
b1n <120295547+b1nhack@users.noreply.github.com>
2024-05-13 17:09:09 +0800

    refactor(telescope): refactor code

--- lua/telescope-function.lua
+++ lua/telescope-function.lua
@@ -1,21 +1,17 @@
--- telescope-config.lua
 local M = {}
 
--- We cache the results of "git rev-parse"
--- Process creation is expensive in Windows, so this reduces latency
-local is_inside_work_tree = {}
+local builtin = require("telescope.builtin")
+
+local function is_git_repo()
+	vim.fn.system("git rev-parse --is-inside-work-tree")
+
+	return vim.v.shell_error == 0
+end
 
 M.project_files = function()
-	local builtin = require("telescope.builtin")
 	local opts = {} -- define here if you want to define something
 
-	local cwd = vim.fn.getcwd()
-	if is_inside_work_tree[cwd] == nil then
-		vim.fn.system("git rev-parse --is-inside-work-tree")
-		is_inside_work_tree[cwd] = vim.v.shell_error == 0
-	end
-
-	if is_inside_work_tree[cwd] then
+	if is_git_repo then
 		builtin.git_files(opts)
 	else
 		builtin.find_files(opts)
@@ -23,12 +19,6 @@ M.project_files = function()
 end
 
 M.live_grep_from_project_git_root = function()
-	local function is_git_repo()
-		vim.fn.system("git rev-parse --is-inside-work-tree")
-
-		return vim.v.shell_error == 0
-	end
-
 	local function get_git_root()
 		local dot_git_path = vim.fn.finddir(".git", ".;")
 		return vim.fn.fnamemodify(dot_git_path, ":h")
@@ -42,13 +32,7 @@ M.live_grep_from_project_git_root = function()
 		}
 	end
 
-	require("telescope.builtin").live_grep(opts)
+	builtin.live_grep(opts)
 end
 
 return M
-
--- call via:
--- :lua require"telescope-config".project_files()
-
--- example keymap:
--- vim.api.nvim_set_keymap("n", "<Leader><Space>", "<CMD>lua require'telescope-config'.project_files()<CR>", {noremap = true, silent = true})
