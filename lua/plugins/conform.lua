145650890752daa6a0f2d75bb638da45ac4c1959
b1n <120295547+b1nhack@users.noreply.github.com>
2024-05-14 03:55:38 +0800

    refactor(conform): change settings

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -14,16 +14,10 @@ return {
 				rust = { "rustfmt" },
 				c = { "clang-format" },
 			},
-
-			format_on_save = {
-				-- These options will be passed to conform.format()
-				async = true,
-				lsp_fallback = false,
-			},
 		})
 
 		vim.keymap.set("n", "<Leader>f", function()
-			require("conform").format()
+			require("conform").format({ async = true })
 		end)
 	end,
 }
