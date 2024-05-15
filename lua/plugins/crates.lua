2a051a9f7e5b2d8f6a22cd6d2eb8ced300a19c28
b1n <120295547+b1nhack@users.noreply.github.com>
2024-05-15 15:06:03 +0800

    style: <leader> to <Leader>

--- lua/plugins/crates.lua
+++ lua/plugins/crates.lua
@@ -12,23 +12,23 @@ return {
 					-- the same on_attach function as for your other lsp's
 					local crates = require("crates")
 
-					vim.keymap.set("n", "<leader>ct", crates.toggle, { buffer = bufnr })
-					vim.keymap.set("n", "<leader>cr", crates.reload, { buffer = bufnr })
+					vim.keymap.set("n", "<Leader>ct", crates.toggle, { buffer = bufnr })
+					vim.keymap.set("n", "<Leader>cr", crates.reload, { buffer = bufnr })
 
-					vim.keymap.set("n", "<leader>ci", crates.show_popup, { buffer = bufnr })
-					vim.keymap.set("n", "<leader>cv", crates.show_versions_popup, { buffer = bufnr })
-					vim.keymap.set("n", "<leader>cf", crates.show_features_popup, { buffer = bufnr })
-					vim.keymap.set("n", "<leader>cd", crates.show_dependencies_popup, { buffer = bufnr })
+					vim.keymap.set("n", "<Leader>ci", crates.show_popup, { buffer = bufnr })
+					vim.keymap.set("n", "<Leader>cv", crates.show_versions_popup, { buffer = bufnr })
+					vim.keymap.set("n", "<Leader>cf", crates.show_features_popup, { buffer = bufnr })
+					vim.keymap.set("n", "<Leader>cd", crates.show_dependencies_popup, { buffer = bufnr })
 
-					vim.keymap.set("n", "<leader>cu", crates.update_crate, { buffer = bufnr })
-					vim.keymap.set("v", "<leader>cu", crates.update_crates, { buffer = bufnr })
-					vim.keymap.set("n", "<leader>ca", crates.update_all_crates, { buffer = bufnr })
-					vim.keymap.set("n", "<leader>cU", crates.upgrade_crate, { buffer = bufnr })
-					vim.keymap.set("v", "<leader>cU", crates.upgrade_crates, { buffer = bufnr })
-					vim.keymap.set("n", "<leader>cA", crates.upgrade_all_crates, { buffer = bufnr })
+					vim.keymap.set("n", "<Leader>cu", crates.update_crate, { buffer = bufnr })
+					vim.keymap.set("v", "<Leader>cu", crates.update_crates, { buffer = bufnr })
+					vim.keymap.set("n", "<Leader>ca", crates.update_all_crates, { buffer = bufnr })
+					vim.keymap.set("n", "<Leader>cU", crates.upgrade_crate, { buffer = bufnr })
+					vim.keymap.set("v", "<Leader>cU", crates.upgrade_crates, { buffer = bufnr })
+					vim.keymap.set("n", "<Leader>cA", crates.upgrade_all_crates, { buffer = bufnr })
 
-					vim.keymap.set("n", "<leader>cx", crates.expand_plain_crate_to_inline_table, { buffer = bufnr })
-					vim.keymap.set("n", "<leader>cX", crates.extract_crate_into_table, { buffer = bufnr })
+					vim.keymap.set("n", "<Leader>cx", crates.expand_plain_crate_to_inline_table, { buffer = bufnr })
+					vim.keymap.set("n", "<Leader>cX", crates.extract_crate_into_table, { buffer = bufnr })
 				end,
 				actions = true,
 				completion = true,
