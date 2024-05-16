c7846dd12e339c79c1d619c0665addaf7ed8e7a5
b1n <120295547+b1nhack@users.noreply.github.com>
2024-05-16 15:34:49 +0800

    perf(illuminate): optimize large file performance

--- lua/plugins/vim-illuminate.lua
+++ lua/plugins/vim-illuminate.lua
@@ -1,4 +1,14 @@
 return {
 	"RRethy/vim-illuminate",
 	event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+
+	config = function()
+		require("illuminate").configure({
+			delay = 199,
+			large_file_cutoff = 1999,
+			large_file_overrides = {
+				providers = { "lsp" },
+			},
+		})
+	end,
 }
