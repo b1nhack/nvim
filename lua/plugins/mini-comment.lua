079b00ab23bdcc648ff2404a96207f666af04941
b1n <120295547+b1nhack@users.noreply.github.com>
2024-05-16 15:19:00 +0800

    feat(comment): comment injected language

--- lua/plugins/mini-comment.lua
+++ lua/plugins/mini-comment.lua
@@ -1,9 +1,18 @@
 return {
 	"echasnovski/mini.comment",
 	version = "*",
+	dependencies = {
+		"nvim-ts-context-commentstring",
+	},
 	event = { "BufReadPost", "BufWritePost", "BufNewFile" },
 
 	config = function()
-		require("mini.comment").setup()
+		require("mini.comment").setup({
+			options = {
+				custom_commentstring = function()
+					return require("ts_context_commentstring").calculate_commentstring() or vim.bo.commentstring
+				end,
+			},
+		})
 	end,
 }
