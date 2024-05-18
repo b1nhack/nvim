108b8d9393d08d657cc13ba83fc3551f06667dec
b1n <120295547+b1nhack@users.noreply.github.com>
2024-05-18 18:35:07 +0800

    feat(ufo): disable foldcolumn

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -20,12 +20,10 @@ return {
 	},
 
 	config = function()
-		vim.o.foldcolumn = "1" -- '0' is not bad
+		vim.o.foldcolumn = "0" -- '0' is not bad
 		vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 		vim.o.foldlevelstart = 99
 		vim.o.foldenable = true
-
-		vim.o.fillchars = [[foldopen:,foldsep: ,foldclose:]]
 
 		local handler = function(virtText, lnum, endLnum, width, truncate)
 			local newVirtText = {}
