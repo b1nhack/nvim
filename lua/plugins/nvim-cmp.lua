7ddd15d2de2c43fd58c742e263ba8682cd091a66
b1n <120295547+b1nhack@users.noreply.github.com>
2024-05-18 22:33:32 +0800

    feat(cmp): change keymap

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -71,8 +71,20 @@ return {
 			}),
 
 			mapping = {
-				["<C-u>"] = cmp.mapping.scroll_docs(-5), -- Up
-				["<C-e>"] = cmp.mapping.scroll_docs(5), -- Down
+				["<C-u>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					else
+						fallback()
+					end
+				end),
+				["<C-e>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					else
+						fallback()
+					end
+				end),
 
 				["<CR>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
@@ -87,21 +99,30 @@ return {
 						fallback()
 					end
 				end),
+				["<S-CR>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						if luasnip.expandable() then
+							luasnip.expand()
+						else
+							cmp.confirm({
+								select = true,
+								behavior = cmp.ConfirmBehavior.Replace,
+							})
+						end
+					else
+						fallback()
+					end
+				end),
 
 				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.locally_jumpable(1) then
+					if luasnip.locally_jumpable(1) then
 						luasnip.jump(1)
 					else
 						fallback()
 					end
 				end, { "i", "s" }),
-
 				["<S-Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.locally_jumpable(-1) then
+					if luasnip.locally_jumpable(-1) then
 						luasnip.jump(-1)
 					else
 						fallback()
