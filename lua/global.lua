6cfdb1133887e3fea5a3aee02714aa89aac303ed
b1n <120295547+b1nhack@users.noreply.github.com>
2024-05-20 00:41:32 +0800

    refactor: global icons

---
+++ lua/global.lua
@@ -0,0 +1,43 @@
+local M = {}
+
+M.icons = {
+	Text = "󰦨 ",
+	Method = " ",
+	Function = "󰡱 ",
+	Constructor = " ",
+	Field = " ",
+	Variable = " ",
+	Class = " ",
+	Interface = " ",
+	Module = "󰰏 ",
+	Property = " ",
+	Unit = "󰰧 ",
+	Value = " ",
+	Enum = " ",
+	Keyword = " ",
+	Snippet = " ",
+	Color = " ",
+	File = " ",
+	Reference = " ",
+	Folder = " ",
+	EnumMember = " ",
+	Constant = " ",
+	Struct = " ",
+	Event = " ",
+	Operator = " ",
+	TypeParameter = " ",
+	Array = " ",
+	Boolean = " ",
+	Key = " ",
+	Namespace = " ",
+	Null = "󰟢 ",
+	Number = " ",
+	Object = "󰰕 ",
+	Package = " ",
+	String = " ",
+
+	Version = "󰰪 ",
+	Feature = " ",
+}
+
+return M
