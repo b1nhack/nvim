b783c0001facd9e8d38901fc2e79c9bf99ea73fb
b1n <120295547+b1nhack@users.noreply.github.com>
2024-05-20 01:45:38 +0800

    feat(comment): support block comment
    Change to Comment.nvim

---
+++ lua/plugins/Comment.lua
@@ -0,0 +1,17 @@
+return {
+	"numToStr/Comment.nvim",
+	event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+
+	config = function()
+		---@diagnostic disable-next-line: missing-fields
+		require("Comment").setup({
+			---@diagnostic disable-next-line: missing-fields
+			extra = {
+				---Add comment at the end of line
+				eol = "gci",
+			},
+
+			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+		})
+	end,
+}
