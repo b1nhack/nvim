e58950c08cf97a76088d030ca1d55a18812ede11
b1n <b1nhack@proton.me>
2024-05-26 22:46:08 +0800

    feat(catppuccin): change settings

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -10,7 +10,7 @@ return {
 			dim_inactive = {
 				enabled = true, -- dims the background color of inactive window
 				shade = "dark",
-				percentage = 0.15, -- percentage of the shade to apply to the inactive window
+				percentage = 0.19, -- percentage of the shade to apply to the inactive window
 			},
 
 			default_integrations = false,
