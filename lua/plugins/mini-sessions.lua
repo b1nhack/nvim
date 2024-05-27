d37e022a0f9abf37acf931cbae55237fc434b26a
b1n <b1nhack@proton.me>
2024-05-27 13:54:14 +0800

    feat(sessions): restrict names to alphanumeric

--- lua/plugins/mini-sessions.lua
+++ lua/plugins/mini-sessions.lua
@@ -7,6 +7,19 @@ return {
 	},
 
 	config = function()
+		local function check_string(str)
+			if str == nil or str == "" then
+				vim.notify("SessionName cannot be empty", vim.log.levels.WARN)
+				return false
+			end
+
+			if str:match("%W") then
+				vim.notify("SessionName can only contain alphanumeric", vim.log.levels.WARN)
+				return false
+			end
+			return true
+		end
+
 		require("mini.sessions").setup({
 			file = "",
 			force = { read = false, write = true, delete = true },
@@ -14,7 +27,9 @@ return {
 
 		vim.keymap.set("n", "<Leader>kw", function()
 			local name = vim.fn.input("SessionName: ")
-			MiniSessions.write(name)
+			if check_string(name) then
+				MiniSessions.write(name)
+			end
 		end)
 
 		vim.keymap.set("n", "<Leader>ks", function()
