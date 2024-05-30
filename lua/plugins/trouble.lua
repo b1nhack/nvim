ef6ea3015ee6eab424c591c82bec18ec1091a9ca
b1n <b1nhack@proton.me>
2024-05-30 21:50:50 +0800

    chore: update plugins

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,6 +1,5 @@
 return {
 	"folke/trouble.nvim",
-	branch = "dev", -- IMPORTANT!
 	keys = {
 		"<Leader>fs",
 		"<Leader>fd",
@@ -21,49 +20,6 @@ return {
 
 		require("trouble").setup({
 			focus = true,
-			pinned = true,
-
-			-- Key mappings can be set to the name of a builtin action,
-			-- or you can define your own custom action.
-			---@type table<string, string|trouble.Action>
-			keys = {
-				["?"] = "help",
-				r = "refresh",
-				R = "toggle_refresh",
-				q = "close",
-				o = "jump_close",
-				["<esc>"] = "close",
-				["<cr>"] = "jump",
-				-- ["<2-leftmouse>"] = "jump",
-				["<c-s>"] = "jump_split",
-				["<c-v>"] = "jump_vsplit",
-				-- go down to next item (accepts count)
-				-- e = "next",
-				["}"] = "next",
-				["]]"] = "next",
-				-- go up to prev item (accepts count)
-				-- u = "prev",
-				["{"] = "prev",
-				["[["] = "prev",
-				i = "inspect",
-				p = "preview",
-				P = "toggle_preview",
-				zo = "fold_open",
-				zO = "fold_open_recursive",
-				zc = "fold_close",
-				zC = "fold_close_recursive",
-				za = "fold_toggle",
-				zA = "fold_toggle_recursive",
-				zm = "fold_more",
-				zM = "fold_close_all",
-				zr = "fold_reduce",
-				zR = "fold_open_all",
-				zx = "fold_update",
-				zX = "fold_update_all",
-				zn = "fold_disable",
-				zN = "fold_enable",
-				zi = "fold_toggle_enable",
-			},
 
 			---@type table<string, trouble.Mode>
 			modes = {
