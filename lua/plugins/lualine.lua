b33fb5eff8449ee207cad31e205c37910af66120
b1n <b1n@b1n.io>
2024-06-12 14:31:53 +0800

    feat(lualine): change clock icon

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -116,7 +116,7 @@ return {
 					{
 						"datetime",
 						-- options: default, us, uk, iso, or your own format string ("%H:%M", etc..)
-						style = "󰥔 %H:%M",
+						style = " %H:%M",
 						separator = { left = "", right = "" },
 					},
 				},
