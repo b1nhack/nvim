d02c34e15033aa432ec7b1e2e8f090215076060b
b1n <b1n@b1n.io>
2024-06-19 11:42:16 +0800

    feat(nvim-lspconfig): enable severity_sort

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -4,24 +4,25 @@ return {
 	init = function()
 		vim.diagnostic.config({
 			virtual_text = false,
+			severity_sort = true,
 			signs = {
 				text = {
 					[vim.diagnostic.severity.ERROR] = " ",
 					[vim.diagnostic.severity.WARN] = " ",
-					[vim.diagnostic.severity.HINT] = "󰌶 ",
 					[vim.diagnostic.severity.INFO] = " ",
+					[vim.diagnostic.severity.HINT] = "󰌶 ",
 				},
 				texthl = {
 					[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
 					[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
-					[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
 					[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+					[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
 				},
 				numhl = {
 					[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
 					[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
-					[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
 					[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+					[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
 				},
 			},
 		})
