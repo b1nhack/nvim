a208cd7ced2d959568955256fd70c2557b89b9c6
b1n <b1n@b1n.io>
2024-07-13 10:18:52 +0800

    feat(gitsigns): disable signs_staged_enable

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -12,6 +12,21 @@ return {
 				changedelete = { text = " " },
 				untracked = { text = "┆" },
 			},
+			signs_staged = {
+				add = { text = " " },
+				change = { text = " " },
+				delete = { text = " " },
+				topdelete = { text = " " },
+				changedelete = { text = " " },
+				untracked = { text = "┆" },
+			},
+			signs_staged_enable = false,
+			attach_to_untracked = true,
+
+			preview_config = {
+				-- Options passed to nvim_open_win
+				border = "rounded",
+			},
 
 			on_attach = function(bufnr)
 				local gitsigns = require("gitsigns")
@@ -63,10 +78,6 @@ return {
 				-- Text object
 				map({ "o", "x" }, "kh", ":<C-U>Gitsigns select_hunk<CR>", { silent = true })
 			end,
-			preview_config = {
-				-- Options passed to nvim_open_win
-				border = "rounded",
-			},
 		})
 	end,
 }
