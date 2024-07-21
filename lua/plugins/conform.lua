47dcfaba9184129b6b89231b8f5ae44e5229c7fb
b1n <b1n@b1n.io>
2024-07-22 07:04:50 +0800

    refactor(conform): follow upstream

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,28 +6,35 @@ return {
 	},
 
 	init = function()
+		-- If you want the formatexpr, here is the place to set it
 		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
 	end,
 
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				c = { "clang-format" },
-				lua = { "stylua" },
-				markdown = { "mdformat" },
-				rust = { "rustfmt" },
-				sh = { "shfmt" },
-				zsh = { "shfmt" },
+	-- This will provide type hinting with LuaLS
+	---@module "conform"
+	---@type conform.setupOpts
+	opts = {
+		-- Define your formatters
+		formatters_by_ft = {
+			c = { "clang-format" },
+			lua = { "stylua" },
+			markdown = { "mdformat" },
+			rust = { "rustfmt" },
+			sh = { "shfmt" },
+			zsh = { "shfmt" },
 
-				["*"] = { "injected" },
-			},
+			["*"] = { "injected" },
+		},
 
-			format_on_save = {
-				-- I recommend these options. See :help conform.format for details.
-				lsp_format = "fallback",
-				timeout_ms = 500,
-				undojoin = true,
-			},
-		})
-	end,
+		-- Set default options
+		default_format_opts = {
+			lsp_format = "fallback",
+			undojoin = true,
+		},
+
+		-- Set up format-on-save
+		format_on_save = {
+			timeout_ms = 500,
+		},
+	},
 }
