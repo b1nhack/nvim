e28d1c9a42f9e587d365939c248b7f549552a59d
b1n <b1n@b1n.io>
2024-08-05 08:38:09 +0800

    feat(markdown): disable latex

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -2,6 +2,10 @@ return {
 	"MeanderingProgrammer/markdown.nvim",
 	main = "render-markdown",
 	ft = "markdown",
-	opts = {},
+	opts = {
+		latex = {
+			enabled = false,
+		},
+	},
 	dependencies = { "nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
 }
