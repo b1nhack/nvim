a1a470be7122205a01f9b4034d471cce9529043f
b1n <b1n@b1n.io>
2024-08-30 04:22:22 +0800

    feat(lsp): lua not only for neovim

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -6,37 +6,6 @@ local lspconfig = require('lspconfig')
 M.lua_ls = function()
   lspconfig.lua_ls.setup({
     capabilities = capabilities,
-
-    on_init = function(client)
-      local path = client.workspace_folders[1].name
-      if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
-        return
-      end
-
-      client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-        runtime = {
-          -- Tell the language server which version of Lua you're using
-          -- (most likely LuaJIT in the case of Neovim)
-          version = 'LuaJIT',
-        },
-        -- Make the server aware of Neovim runtime files
-        workspace = {
-          checkThirdParty = false,
-          library = {
-            vim.env.VIMRUNTIME,
-            -- Depending on the usage, you might want to add additional paths here.
-            -- "${3rd}/luv/library"
-            -- "${3rd}/busted/library",
-          },
-          -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-          -- library = vim.api.nvim_get_runtime_file("", true)
-        },
-      })
-    end,
-    settings = {
-      Lua = {},
-    },
-
     root_dir = lspconfig.util.root_pattern(
       '.luarc.json',
       '.luarc.jsonc',
@@ -48,6 +17,13 @@ M.lua_ls = function()
       '.git',
       'lazy-lock.json'
     ),
+    settings = {
+      Lua = {
+        runtime = {
+          version = 'LuaJIT',
+        },
+      },
+    },
   })
 end
 
