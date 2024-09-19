2d19d848a5f055a041abb524aa919cf8596ca122
Xingquan Liu <b1n@b1n.io>
2024-09-20 03:14:09 +0800

    feat(oil): add gs keymap to open grug-far

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,7 +5,9 @@ return {
   event = 'VeryLazy',
 
   config = function()
-    require('oil').setup({
+    local oil = require('oil')
+
+    oil.setup({
       columns = {
         -- "icon",
         -- "permissions",
@@ -23,9 +25,17 @@ return {
       keymaps = {
         ['?'] = 'actions.show_help',
         ['<CR>'] = 'actions.select',
-        ['<C-v>'] = 'actions.select_vsplit',
-        ['<C-s>'] = 'actions.select_split',
-        ['<C-t>'] = 'actions.select_tab',
+        ['<C-v>'] = {
+          'actions.select',
+          opts = { vertical = true },
+          desc = 'Open the entry in a vertical split',
+        },
+        ['<C-s>'] = {
+          'actions.select',
+          opts = { horizontal = true },
+          desc = 'Open the entry in a horizontal split',
+        },
+        ['<C-t>'] = { 'actions.select', opts = { tab = true }, desc = 'Open the entry in new tab' },
         ['<C-p>'] = 'actions.preview',
         ['<C-c>'] = 'actions.close',
         ['r'] = 'actions.refresh',
@@ -33,7 +43,28 @@ return {
         ['_'] = 'actions.open_cwd',
         ['`'] = false,
         ['~'] = false,
-        ['gs'] = false,
+        -- create a new mapping, gs, to search and replace in the current directory
+        gs = {
+          callback = function()
+            -- get the current directory
+            local prefills = { paths = oil.get_current_dir() }
+
+            local grug_far = require('grug-far')
+            -- instance check
+            if not grug_far.has_instance('explorer') then
+              grug_far.open {
+                instanceName = 'explorer',
+                prefills = prefills,
+                staticTitle = 'Find and Replace from Explorer',
+              }
+            else
+              grug_far.open_instance('explorer')
+              -- updating the prefills without clearing the search and other fields
+              grug_far.update_instance_prefills('explorer', prefills, false)
+            end
+          end,
+          desc = 'oil: Search in directory',
+        },
         ['gx'] = false,
         ['g.'] = 'actions.toggle_hidden',
         ['g\\'] = false,
