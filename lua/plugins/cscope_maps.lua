328091292b7cca05c1d477264afb79bc37a4e7a0
Xingquan Liu <b1n@b1n.io>
2024-09-27 05:40:55 +0800

    feat(cscope_maps): disable skip_input_prompt

--- lua/plugins/cscope_maps.lua
+++ lua/plugins/cscope_maps.lua
@@ -2,7 +2,6 @@ return {
   'dhananjaylatkar/cscope_maps.nvim',
   event = 'VeryLazy',
   opts = {
-    skip_input_prompt = true,
     cscope = {
       picker = 'telescope',
       skip_picker_for_single_result = true,
