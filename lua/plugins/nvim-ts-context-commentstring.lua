742c38c4d59a12c6eb070c0c0c791aaf403afc3f
Xingquan Liu <b1n@b1n.io>
2024-10-03 12:07:22 +0800

    feat(nvim-ts-context-commentstring): disable skip_ts_context_commentstring_module

--- lua/plugins/nvim-ts-context-commentstring.lua
+++ lua/plugins/nvim-ts-context-commentstring.lua
@@ -1,12 +1,7 @@
 return {
   'JoosepAlviste/nvim-ts-context-commentstring',
 
-  init = function()
-    vim.g.skip_ts_context_commentstring_module = true
-  end,
-
   config = function()
-    ---@diagnostic disable-next-line: missing-fields
     require('ts_context_commentstring').setup({
       enable_autocmd = false,
     })
