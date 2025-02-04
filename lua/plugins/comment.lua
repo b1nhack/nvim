8aa5dd96cdea9fa6ec1c06cb2ea3965712fc84fa
Xingquan Liu <b1n@b1n.io>
2025-02-04 15:32:44 +0800

    feat(comment): change eol keymap to gcI

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,15 +1,10 @@
 return {
   'numToStr/Comment.nvim',
   event = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
-
-  config = function()
-    ---@diagnostic disable-next-line: missing-fields
-    require('Comment').setup({
-      ---@diagnostic disable-next-line: missing-fields
-      extra = {
-        ---Add comment at the end of line
-        eol = 'gce',
-      },
-    })
-  end,
+  opts = {
+    extra = {
+      ---Add comment at the end of line
+      eol = 'gcI',
+    },
+  },
 }
