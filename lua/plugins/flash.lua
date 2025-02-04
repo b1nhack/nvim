962cc7a01ff6de0d42424af5ad3b80cae0c2b1d9
Xingquan Liu <b1n@b1n.io>
2025-02-04 14:48:34 +0800

    style(flash): adjust indent

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,14 +1,14 @@
 return {
   'folke/flash.nvim',
   event = 'VeryLazy',
-		-- stylua: ignore
-	keys = {
-		{ "s", function() require("flash").jump() end, mode = { "n", "x", "o" }, desc = "Flash", },
-		{ "S", function() require("flash").treesitter() end, mode = { "n", "x", "o" }, desc = "Flash Treesitter", },
-		{ "r", function() require("flash").remote() end, mode = "o", desc = "Remote Flash", },
-		{ "R", function() require("flash").treesitter_search() end, mode = { "o", "x" }, desc = "Treesitter Search", },
-		{ "<C-s>", function() require("flash").toggle() end, mode = { "c" }, desc = "Toggle Flash Search", },
-	},
+  -- stylua: ignore
+  keys = {
+    { "s", function() require("flash").jump() end, mode = { "n", "x", "o" }, desc = "Flash", },
+    { "S", function() require("flash").treesitter() end, mode = { "n", "x", "o" }, desc = "Flash Treesitter", },
+    { "r", function() require("flash").remote() end, mode = "o", desc = "Remote Flash", },
+    { "R", function() require("flash").treesitter_search() end, mode = { "o", "x" }, desc = "Treesitter Search", },
+    { "<C-s>", function() require("flash").toggle() end, mode = { "c" }, desc = "Toggle Flash Search", },
+  },
 
   config = function()
     require('flash').setup({
