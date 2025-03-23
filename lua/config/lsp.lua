e0d73511303a913a9435e9b8ed5447ab5a8c0014
Xingquan Liu <b1n@b1n.io>
2025-03-23 17:56:31 +0800

    feat(mason-lspconfig): use default lua_ls config

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -3,30 +3,6 @@ local M = {}
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lspconfig = require('lspconfig')
 
-M.lua_ls = function()
-  lspconfig.lua_ls.setup({
-    capabilities = capabilities,
-    root_dir = lspconfig.util.root_pattern(
-      '.luarc.json',
-      '.luarc.jsonc',
-      '.luacheckrc',
-      '.stylua.toml',
-      'stylua.toml',
-      'selene.toml',
-      'selene.yml',
-      '.git',
-      'lazy-lock.json'
-    ),
-    settings = {
-      Lua = {
-        runtime = {
-          version = 'LuaJIT',
-        },
-      },
-    },
-  })
-end
-
 M.nixd = function()
   lspconfig.nixd.setup({
     capabilities = capabilities,
--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -9,18 +9,13 @@ return {
 
   config = function()
     local lsp = require('config.lsp')
+    lsp.nixd()
 
     require('mason-lspconfig').setup_handlers({
       function(server_name)
         require('lspconfig')[server_name].setup({
           capabilities = require('cmp_nvim_lsp').default_capabilities(),
         })
-      end,
-
-      lsp.nixd(),
-
-      ['lua_ls'] = function()
-        lsp.lua_ls()
       end,
     })
   end,
