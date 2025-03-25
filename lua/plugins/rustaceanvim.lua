b8854e3af16db8436e387351ce97dfd6591522c5
Xingquan Liu <b1n@b1n.io>
2025-03-25 21:34:44 +0800

    feat(rustaceanvim): lazyload by filetype

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -1,7 +1,7 @@
 return {
   'mrcjkb/rustaceanvim',
   version = '^5', -- Recommended
-  lazy = false, -- This plugin is already lazy
+  ft = 'rust',
 
   config = function()
     vim.g.rustaceanvim = {
