c9ed3adcbf0a4437dc99b2b7b28b5e17767f4564
Xingquan Liu <b1n@b1n.io>
2025-04-27 09:03:40 -0400

    feat(conform): disable autoformat for C project

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,27 +1,28 @@
-vim.api.nvim_create_user_command('FormatDisable', function(args)
-  if args.bang then
-    -- FormatDisable! will disable formatting just for this buffer
-    vim.b.disable_autoformat = true
-  else
-    vim.g.disable_autoformat = true
-  end
-end, {
-  desc = 'Disable autoformat-on-save',
-  bang = true,
-})
-
-vim.api.nvim_create_user_command('FormatEnable', function()
-  vim.b.disable_autoformat = false
-  vim.g.disable_autoformat = false
-end, {
-  desc = 'Re-enable autoformat-on-save',
-})
-
 return {
   'stevearc/conform.nvim',
   event = 'BufWritePre',
-  cmd = {
-    'ConformInfo',
+  cmd = 'ConformInfo',
+  -- https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#leave-visual-mode-after-range-format
+  keys = {
+    {
+      '<Leader>f',
+      function()
+        require('conform').format({ async = true }, function(err)
+          if not err then
+            local mode = vim.api.nvim_get_mode().mode
+            if vim.startswith(string.lower(mode), 'v') then
+              vim.api.nvim_feedkeys(
+                vim.api.nvim_replace_termcodes('<Esc>', true, false, true),
+                'n',
+                true
+              )
+            end
+          end
+        end)
+      end,
+      mode = { 'n', 'x' },
+      desc = 'Format buffer',
+    },
   },
 
   init = function()
@@ -53,8 +54,9 @@ return {
 
     -- Set up format-on-save
     format_on_save = function(bufnr)
-      -- Disable with a global or buffer-local variable
-      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+      -- Disable autoformat on certain filetypes
+      local ignore_filetypes = { 'c', 'h' }
+      if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
         return
       end
       return { timeout_ms = 500, lsp_format = 'fallback' }
