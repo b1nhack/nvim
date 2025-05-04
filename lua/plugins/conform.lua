06c392dfbcaf8857f07e0977a361d6593e8267b3
Xingquan Liu <b1n@b1n.io>
2025-05-04 06:21:14 -0400

    feat(conform): add command to toggle format-on-save

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,3 +1,21 @@
+vim.api.nvim_create_user_command('FormatDisable', function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = 'Disable autoformat-on-save',
+  bang = true,
+})
+vim.api.nvim_create_user_command('FormatEnable', function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = 'Re-enable autoformat-on-save',
+})
+
 return {
   'stevearc/conform.nvim',
   event = 'BufWritePre',
@@ -57,6 +75,11 @@ return {
       -- Disable autoformat on certain filetypes
       local ignore_filetypes = { 'c', 'h' }
       if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+        return
+      end
+
+      -- Disable with a global or buffer-local variable
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
         return
       end
       return { timeout_ms = 500, lsp_format = 'fallback' }
