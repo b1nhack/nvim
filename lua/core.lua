8f3245ec0e444f3388c31041e2df9b4fc66280a0
Xingquan Liu <b1n@b1n.io>
2025-07-07 14:52:10 -0400

    feat(nvim-treesitter): update to main branch

--- lua/core.lua
+++ lua/core.lua
@@ -20,7 +20,6 @@ vim.o.infercase = true
 vim.o.wildignorecase = true
 
 vim.o.foldmethod = 'expr'
-vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 vim.o.foldtext = ''
 vim.o.foldcolumn = '0'
 vim.o.foldlevel = 99
--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,16 +1,65 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  -- TODO: remove when default branch is main
+  branch = 'main',
   build = ':TSUpdate',
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-  },
-  event = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
-  main = 'nvim-treesitter.configs',
 
-  ---@module 'nvim-treesitter.configs'
-  ---@type TSConfig
-  opts = {
-    ensure_installed = {
+  config = function()
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = {
+        -- bash
+        'sh',
+        'zsh',
+        -- c
+        'c',
+        -- diff
+        'diff',
+        -- git_rebase
+        'gitrebase',
+        -- gitcommit
+        'gitcommit',
+        -- html
+        'html',
+        -- ini
+        'dosini',
+        -- json
+        'json',
+        -- jsonc
+        'jsonc',
+        -- lua
+        'lua',
+        -- markdown markdown_inline
+        'markdown',
+        -- nix
+        'nix',
+        -- python
+        'python',
+        -- query
+        'scheme',
+        -- regex
+        'regex',
+        -- rust
+        'rust',
+        -- toml
+        'toml',
+        -- vim
+        'vim',
+        -- vimdoc
+        'vimdoc',
+      },
+
+      callback = function()
+        -- syntax highlighting, provided by Neovim
+        vim.treesitter.start()
+        -- folds, provided by Neovim
+        vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+        -- indentation, provided by nvim-treesitter
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+
+    require('nvim-treesitter').install({
       'bash',
       'c',
       'diff',
@@ -31,71 +80,10 @@ return {
       'toml',
       'vim',
       'vimdoc',
-    },
+    })
 
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-      disable = function(_, bufnr)
-        return vim.api.nvim_buf_line_count(bufnr) > 9999
-      end,
-    },
-
-    incremental_selection = {
-      enable = false,
-    },
-
-    indent = {
-      enable = true,
-    },
-
-    textobjects = {
-      select = {
-        enable = true,
-
-        -- Automatically jump forward to textobj, similar to targets.vim
-        lookahead = true,
-
-        keymaps = {
-          ['af'] = '@function.outer',
-          ['kf'] = '@function.inner',
-          ['ab'] = '@block.outer',
-          ['kb'] = '@block.inner',
-          ['aa'] = '@parameter.outer',
-          ['ka'] = '@parameter.inner',
-          ['ac'] = '@comment.outer',
-        },
-
-        -- If you set this to `true` (default is `false`) then any textobject is
-        -- extended to include preceding or succeeding whitespace. Succeeding
-        -- whitespace has priority in order to act similarly to eg the built-in
-        -- `ap`.
-        --
-        -- Can also be a function which gets passed a table with the keys
-        -- * query_string: eg '@function.inner'
-        -- * selection_mode: eg 'v'
-        -- and should return true or false
-        include_surrounding_whitespace = false,
-      },
-
-      swap = {
-        enable = false,
-      },
-
-      move = {
-        enable = true,
-        set_jumps = false, -- whether to set jumps in the jumplist
-        goto_next_start = {
-          [']a'] = '@parameter.outer',
-          [']f'] = '@function.outer',
-          [']c'] = '@class.outer',
-        },
-        goto_previous_start = {
-          ['[a'] = '@parameter.outer',
-          ['[f'] = '@function.outer',
-          ['[c'] = '@class.outer',
-        },
-      },
-    },
-  },
+    vim.treesitter.language.register('bash', { 'zsh' })
+    vim.treesitter.language.register('ini', { 'dosini' })
+    vim.treesitter.language.register('query', { 'scheme' })
+  end,
 }
