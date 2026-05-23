99c5387c1d7b3c0273bc3ab31c1b5970adf7612d
Xingquan Liu <b1n@b1n.io>
2026-05-23 08:50:41 -0400

    feat: add mason-tool-installer

---
+++ lua/plugins/mason-tool-installer.lua
@@ -0,0 +1,22 @@
+return {
+  'WhoIsSethDaniel/mason-tool-installer.nvim',
+  event = 'VeryLazy',
+  dependencies = { 'mason-org/mason.nvim' },
+
+  opts = {
+    ensure_installed = {
+      -- LSP
+      'clangd',
+      'lua-language-server',
+      'python-lsp-server',
+
+      -- Formatter
+      'clang-format',
+      'stylua',
+      'black',
+      'shfmt',
+    },
+
+    debounce_hours = 24,
+  },
+}
