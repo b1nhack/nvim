098d87743fe4b9be54abb30ef5f2062f9548aa9a
Xingquan Liu <b1n@b1n.io>
2026-05-23 08:28:53 -0400

    feat: add mason

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,53 @@
+return {
+  'mason-org/mason.nvim',
+  event = 'VeryLazy',
+  keys = {
+    { '<Leader>m', '<CMD>Mason<CR>' },
+  },
+  ---@class MasonSettings
+  opts = {
+    ui = {
+      backdrop = 0,
+      icons = {
+        package_installed = '✓',
+        package_pending = '➜',
+        package_uninstalled = '✗',
+      },
+      keymaps = {
+        ---@since 1.0.0
+        -- Keymap to expand a package
+        toggle_package_expand = '<CR>',
+        ---@since 1.0.0
+        -- Keymap to install the package under the current cursor position
+        install_package = 'j',
+        ---@since 1.0.0
+        -- Keymap to reinstall/update the package under the current cursor position
+        update_package = 'o',
+        ---@since 1.0.0
+        -- Keymap to check for new version for the package under the current cursor position
+        check_package_version = 'c',
+        ---@since 1.0.0
+        -- Keymap to update all installed packages
+        update_all_packages = 'O',
+        ---@since 1.0.0
+        -- Keymap to check which installed packages are outdated
+        check_outdated_packages = 'C',
+        ---@since 1.0.0
+        -- Keymap to uninstall a package
+        uninstall_package = 'x',
+        ---@since 1.0.0
+        -- Keymap to cancel a package installation
+        cancel_installation = '<C-c>',
+        ---@since 1.0.0
+        -- Keymap to apply language filter
+        apply_language_filter = '<C-/>',
+        ---@since 1.1.0
+        -- Keymap to toggle viewing package installation log
+        toggle_package_install_log = '<CR>',
+        ---@since 1.8.0
+        -- Keymap to toggle the help view
+        toggle_help = '?',
+      },
+    },
+  },
+}
