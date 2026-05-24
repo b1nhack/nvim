f1b0438e761b300a7be6a4ee3b6bab64ffcd39a6
Xingquan Liu <b1n@b1n.io>
2026-05-24 07:56:12 -0400

    refactor(trouble): use config to setup not opts

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,8 +1,3 @@
-local function lsp_icon(name)
-  local icon, _, _ = require('mini.icons').get('lsp', name)
-  return icon
-end
-
 return {
   'folke/trouble.nvim',
   cmd = 'Trouble',
@@ -10,72 +5,56 @@ return {
     { '<Leader>fs', '<CMD>Trouble symbols toggle focus=true<CR>' },
     { '<Leader>fd', '<CMD>Trouble diagnostics toggle filter.buf=0<CR>' },
     { '<Leader>fD', '<CMD>Trouble diagnostics toggle<CR>' },
-
-    { 'gu', '<CMD>Trouble lsp_outgoing_calls toggle<CR>' },
-    { 'ge', '<CMD>Trouble lsp_incoming_calls toggle<CR>' },
   },
-  specs = {
-    'folke/snacks.nvim',
-    opts = function(_, opts)
-      return vim.tbl_deep_extend('force', opts or {}, {
-        picker = {
-          actions = require('trouble.sources.snacks').actions,
-          win = {
-            input = {
-              keys = {
-                ['<c-q>'] = {
-                  'trouble_open',
-                  mode = { 'n', 'i' },
-                },
-              },
-            },
-          },
+
+  config = function()
+    local function lsp_icon(name)
+      local icon, _, _ = MiniIcons.get('lsp', name)
+      return icon
+    end
+
+    ---@module 'trouble'
+    ---@type trouble.Config
+    require('trouble').setup({
+      focus = true,
+
+      icons = {
+        ---@type trouble.Indent.symbols
+        indent = {
+          fold_open = ' ',
+          fold_closed = ' ',
         },
-      })
-    end,
-  },
-
-  ---@module 'trouble'
-  ---@type trouble.Config
-  opts = {
-    focus = true,
-
-    icons = {
-      ---@type trouble.Indent.symbols
-      indent = {
-        fold_open = ' ',
-        fold_closed = ' ',
+        folder_closed = ' ',
+        folder_open = ' ',
+        kinds = {
+          Array = lsp_icon('array'),
+          Boolean = lsp_icon('boolean'),
+          Class = lsp_icon('class'),
+          Constant = lsp_icon('constant'),
+          Constructor = lsp_icon('constructor'),
+          Enum = lsp_icon('enum'),
+          EnumMember = lsp_icon('enumMember'),
+          Event = lsp_icon('event'),
+          Field = lsp_icon('field'),
+          File = lsp_icon('file'),
+          Function = lsp_icon('function'),
+          Interface = lsp_icon('interface'),
+          Key = lsp_icon('key'),
+          Method = lsp_icon('method'),
+          Module = lsp_icon('module'),
+          Namespace = lsp_icon('namespace'),
+          Null = lsp_icon('null'),
+          Number = lsp_icon('number'),
+          Object = lsp_icon('object'),
+          Operator = lsp_icon('operator'),
+          Package = lsp_icon('package'),
+          Property = lsp_icon('property'),
+          String = lsp_icon('string'),
+          Struct = lsp_icon('struct'),
+          TypeParameter = lsp_icon('typeParameter'),
+          Variable = lsp_icon('variable'),
+        },
       },
-      folder_closed = ' ',
-      folder_open = ' ',
-      kinds = {
-        Array = lsp_icon('array'),
-        Boolean = lsp_icon('boolean'),
-        Class = lsp_icon('class'),
-        Constant = lsp_icon('constant'),
-        Constructor = lsp_icon('constructor'),
-        Enum = lsp_icon('enum'),
-        EnumMember = lsp_icon('enumMember'),
-        Event = lsp_icon('event'),
-        Field = lsp_icon('field'),
-        File = lsp_icon('file'),
-        Function = lsp_icon('function'),
-        Interface = lsp_icon('interface'),
-        Key = lsp_icon('key'),
-        Method = lsp_icon('method'),
-        Module = lsp_icon('module'),
-        Namespace = lsp_icon('namespace'),
-        Null = lsp_icon('null'),
-        Number = lsp_icon('number'),
-        Object = lsp_icon('object'),
-        Operator = lsp_icon('operator'),
-        Package = lsp_icon('package'),
-        Property = lsp_icon('property'),
-        String = lsp_icon('string'),
-        Struct = lsp_icon('struct'),
-        TypeParameter = lsp_icon('typeParameter'),
-        Variable = lsp_icon('variable'),
-      },
-    },
-  },
+    })
+  end,
 }
