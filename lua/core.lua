e33c250e41be939f444f5fc5fd862fc23a72afdc
Xingquan Liu <b1n@b1n.io>
2026-06-06 07:15:10 -0400

    feat(core): disable winborder

--- lua/core.lua
+++ lua/core.lua
@@ -38,7 +38,6 @@ vim.o.cmdheight = 0
 vim.o.signcolumn = 'yes'
 vim.o.termguicolors = true
 vim.o.wildmenu = false
-vim.o.winborder = 'rounded'
 vim.o.wildchar = 0
 vim.o.fillchars = table.concat({
   'horiz:═',
