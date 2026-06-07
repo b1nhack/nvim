bdd3dc9c2cc78335a0188a5a9426cdd04313fd01
Xingquan Liu <b1n@b1n.io>
2026-06-07 12:54:29 -0400

    style(smart-splits): use M as ALT

--- lua/plugins/smart-splits.lua
+++ lua/plugins/smart-splits.lua
@@ -2,20 +2,20 @@ return {
   'mrjones2014/smart-splits.nvim',
   -- stylua: ignore
   keys = {
-    { '<A-u>', function() require('smart-splits').move_cursor_up() end },
-    { '<A-e>', function() require('smart-splits').move_cursor_down() end },
-    { '<A-n>', function() require('smart-splits').move_cursor_left() end },
-    { '<A-i>', function() require('smart-splits').move_cursor_right() end },
+    { '<M-u>', function() require('smart-splits').move_cursor_up() end },
+    { '<M-e>', function() require('smart-splits').move_cursor_down() end },
+    { '<M-n>', function() require('smart-splits').move_cursor_left() end },
+    { '<M-i>', function() require('smart-splits').move_cursor_right() end },
 
     { '<S-Up>', function() require('smart-splits').resize_up() end },
     { '<S-Down>', function() require('smart-splits').resize_down() end },
     { '<S-Left>', function() require('smart-splits').resize_left() end },
     { '<S-Right>', function() require('smart-splits').resize_right() end },
 
-    { '<A-S-u>', function() require('smart-splits').swap_buf_up() end },
-    { '<A-S-e>', function() require('smart-splits').swap_buf_down() end },
-    { '<A-S-n>', function() require('smart-splits').swap_buf_left() end },
-    { '<A-S-i>', function() require('smart-splits').swap_buf_right() end },
+    { '<M-S-u>', function() require('smart-splits').swap_buf_up() end },
+    { '<M-S-e>', function() require('smart-splits').swap_buf_down() end },
+    { '<M-S-n>', function() require('smart-splits').swap_buf_left() end },
+    { '<M-S-i>', function() require('smart-splits').swap_buf_right() end },
   },
   opts = {
     at_edge = 'stop',
